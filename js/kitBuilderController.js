eacce111c022f1eeb6b364dee9059e896fe57d20
Isovitsch, David <ISOVITD@nationwide.com>
2018-07-19 21:28:13 -0400

    Update JSON

--- js/kitBuilderController.js
+++ js/kitBuilderController.js
@@ -6,14 +6,14 @@ app.controller('kitBuilderController', function($scope) {
 
 	$scope.selected = null;
 	$scope.required = [
-					{"name": "RequiredComponent1", "image": "kit1.jpg", "pdf": "kit1-pdf.pdf"},
-					{"name": "RequiredComponent2", "image": "kit2.jpg", "pdf": "kit2-pdf.pdf"},
-					{"name": "RequiredComponent3", "image": "kit3.jpg", "pdf": "kit3-pdf.pdf"}
+					{"name": "Invest for tomorrow - today. Soloist Variable Annuity. Key Facts (NFM-1587AO)", "image": "kit1.jpg", "pdf": "kit1-pdf.pdf"},
+					{"name": "Soloist Product Prospectus (PRO-2503)", "image": "component1.jpg", "pdf": "component1.pdf"},
+					{"name": "Soloist Information (Forms Necessary to Set Up a Simple IRA Plan) (APO-3485)", "image": "component2.jpg", "pdf": "component2.pdf"}
 				];
 	$scope.optional = [
-					{"name": "OptionalComponent1", "image": "kit4.jpg", "pdf": "kit4-pdf.pdf"},
-					{"name": "OptionalComponent2", "image": "kit5.jpg", "pdf": "kit5-pdf.pdf"},
-					{"name": "OptionalComponent3", "image": "kit6.jpg", "pdf": "kit6-pdf.pdf"}
+					{"name": "FI/ID/WH Nationwide Your Life Index Universal Life Client Kit TN", "image": "kit4.jpg", "pdf": "kit4-pdf.pdf"},
+					{"name": "FI ID WH Your Life IUL Protector Client Kit IN", "image": "kit5.jpg", "pdf": "kit5-pdf.pdf"},
+					{"name": "SAVINGS INCENTIVE MATCH PLAN FOR EMPL OF SMALL EMPLOYERS (5304-SIMPLE)", "image": "component3.jpg", "pdf": "component3.pdf"}
 				];
 
 	// Functions
